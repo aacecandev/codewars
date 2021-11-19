f50f5a53c3101726cd5ed58e036f1fcaba26f9bc
Alejandro Aceituna Cano <dev@aacecan.com>
2021-11-19 20:34:48 +0100

    Add 7th/python/invalid-input-error-handling-1.py

---
+++ 7th/python/invalid-input-error-handling-1.py
@@ -0,0 +1,9 @@
+def get_count(words=None):
+    try:
+        assert words, str
+        clean = "".join([ c if c.isalnum() else "" for c in words.lower() ])
+        print(vowels, consonants, clean)
+        return {"vowels": sum(map(lambda letter: 1, filter(lambda letter: letter in 'aeiou', clean))),
+                "consonants": sum(map(lambda letter: 1, filter(lambda letter: letter not in 'aeiou', clean)))}
+    except:
+        return {"vowels": 0, "consonants": 0}
